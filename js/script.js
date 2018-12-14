76aafab4474dff0169cb1909ee55a4f849c9baa6
Lagrik <rain.demorser@gmail.com>
2018-12-14 17:46:19 +0300

    Best of the best

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('#scroll-down').click(function() {
+  $("html, body").animate({ scrollTop: 6200 }, 7000);
+  return false;
+});
+
+/* $('#scroll-up').click(function() {
+     $('html, body').animate({scrollTop: 0},500);
+    });
+*/
